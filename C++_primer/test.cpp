85b7ea4f8679118117ad2c3b3fe0eef301785eaa
LB <mayujiang0814@gmail.com>
2023-10-23 17:26:27 +0800

    1

---
+++ C++_primer/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+/* 
+
+ */ 
+int main()
+{
+    int val = 10;
+    while (val >= 0){
+        std::cout << val << " ";
+        val -= 1;
+    }
+    std::cout << std::endl;
+}  
