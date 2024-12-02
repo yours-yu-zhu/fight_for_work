ad3d849876c7150fccdf607b6eedde08bd491ade
yours-yu-zhu <mayujiang0814@gmail.com>
2024-12-02 11:15:33 +0800

    ✨ feat: delete

--- propject/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/fight_for_work/propject
-# Build directory: /home/ubuntu/fight_for_work/propject/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
